358267cc09ad71e71200f2f776cafa39ad6baeb7
Neeraj Bokde <neerajdhanraj@gmail.com>
2016-08-19 21:05:24 +0000

    version 0.3

--- inst/doc/PSF_vignette.R
+++ inst/doc/PSF_vignette.R
@@ -1,26 +1,68 @@
-## ---- echo=FALSE, results='asis'-----------------------------------------
-data(iris)
-
 ## ------------------------------------------------------------------------
 library(PSF)
 
 ## ------------------------------------------------------------------------
-# Considering `data_in` = iris[1], `w` = 3, `k` = 4 and `next_val` = 5
-pred_for_w(iris[1], 3, 4, 5)
+a <- psf(data = nottem, n.ahead = 12)
+a
 
 ## ------------------------------------------------------------------------
-# Considering `data_in` = iris[1], `w` = 3, `k` = 4 and `next_val` = 5
-pred_for_w_plot(iris[1], 3, 4, 5)
+b <- psf(data = sunspots, n.ahead = 48)
+b
+
+## ---- fig.width = 7, fig.height = 4--------------------------------------
+psf_plot(data = nottem, predictions = a$predictions)
+
+## ---- fig.width = 7, fig.height = 4--------------------------------------
+psf_plot(data = sunspots, predictions = b$predictions)
 
 ## ------------------------------------------------------------------------
-# Considering `data_in` = iris[1] and `next_val` = 5
-optimum_w(iris[1], 5)
+library(PSF)
+library(forecast)
+options(warn=-1)
+  
+## Consider data `sunspots` with removal of last years's readings
+# Training Data
+x <- sunspots[1:2772]
 
-## ------------------------------------------------------------------------
-# Considering `data_in` = iris[1] 
-optimum_k(iris[1])
+# Test Data
+y <- sunspots[2773:2820]
 
-## ------------------------------------------------------------------------
-# Considering `data_in` = iris[1] and next_val = 3
-AUTO_PSF(iris[1],3)
+PSF <- NULL
+ARIMA <- NULL
+ETS <- NULL
+
+for(i in 1:5)
+{
+  set.seed(i)
+  
+  # for PSF
+  a <- psf(data = x, n.ahead = 48)$predictions
+  
+  # for ARIMA
+  b <- forecast(auto.arima(x), 48)$mean
+  
+  # for ets
+  c <- as.numeric(forecast(ets(x), 48)$mean)
+  
+  ## For Error Calculations
+  # Error for PSF
+  PSF[i] <- sqrt(mean((y - a)^2))
+  # Error for ARIMA
+  ARIMA[i] <- sqrt(mean((y - b)^2))
+  # Error for ETS
+  ETS[i] <- sqrt(mean((y - c)^2))
+
+}
+
+## Error values for PSF
+  PSF
+  mean(PSF)
+  
+## Error values for ARIMA
+  ARIMA
+  mean(ARIMA)
+  
+## Error values for ETS
+  ETS
+  mean(ETS)
 
