243826ba47e4045f5993aae3e2b4b064a52e21a4
Neeraj Bokde <neerajdhanraj@gmail.com>
2017-04-23 17:13:50 +0000

    version 0.4

--- R/psf.R
+++ R/psf.R
@@ -1,39 +1,36 @@
-#' Forecasting of univariate time series using the PSF algorithm
+#' Train a PSF model from an univariate time series using the PSF algorithm
 #'
-#' Takes an univariate time series and the prediction horizon as inputs.
+#' Takes an univariate time series as input. Optionally, specific internal parameters of the PSF algorithm can be also specified.
 #' @param data Input univariate time series, in any format (time series (ts), vector, matrix, list, data frame).
-#' @param n.ahead The number of predicted values to be obtained.
 #' @param k The number of clusters, or a vector of candidate values to search for the optimum automatically.
 #' @param w The window size, or a vector of candidate values to search for the optimum automatically.
 #' @param cycle The number of values that conform a cycle in the time series (e.g. 24 hours per day). Only used when input data is not in time series format.
-#' @return A list with 3 elements:
-#' \item{predictions}{Vector with the resulting predictions}
+#' @return An object of class 'psf' with 7 elements:
+#' \item{original_series}{Original time series stored to be used internally to build further plots.}
+#' \item{train_data}{Adapted and normalized internal time series used to train the PSF model.}
 #' \item{k}{Number of clusters used}
 #' \item{w}{Window size used}
+#' \item{cycle}{Determined cycle for the input time series.}
+#' \item{dmin}{Minimum value of the input time series (used to denormalize internally further predictions).}
+#' \item{dmax}{Maximum value of the input time series (used to denormalize internally further predictions).}
 #' @export
 #' @examples
-#' ## Forecast the next 12 values of the univariate time series: nottem (package:datasets).
-#' psf(nottem, 12)
+#' ## Train a PSF model from the univariate time series 'nottem' (package:datasets).
+#' p <- psf(nottem)
 #'
-#' ## Forecast the next 48 values of the univariate time series: sunspots (package:datasets).
-#' psf(sunspots, 48)
-psf <- function (data, n.ahead, k = seq(2,10), w = seq(1,10), cycle = 24) {
+#' ## Train a PSF model from the univariate time series 'sunspots' (package:datasets).
+#' p <- psf(sunspots)
+psf <- function (data, k = seq(2,10), w = seq(1,10), cycle = 24) {
 
   # Step 1. Convert input data to internal format (data.table).
   series = convert_datatype(data)
   if (is.ts(data)) cycle = frequency(data)
 
-  # Step 2. Check integrity of data (both its size and n.ahead must be multiple of cycle).
+  # Step 2. Check integrity of data (its size must be multiple of cycle).
   fit = nrow(series) %% cycle
   if (fit > 0) {
     warning(paste("Time series length is not multiple of", cycle, ". Cutting last", fit, "values!"))
     series = series[1:(.N - fit)]
-  }
-  original.n.ahead = n.ahead
-  fit = n.ahead %% cycle
-  if (fit > 0) {
-    n.ahead = cycle * ceiling(n.ahead / cycle)
-    warning(paste("Prediction horizon is not multiple of", cycle, ". Using", n.ahead, "as prediction horizon!"))
   }
 
   # Step 3. Normalize data.
@@ -51,14 +48,13 @@ psf <- function (data, n.ahead, k = seq(2,10), w = seq(1,10), cycle = 24) {
   if (length(w) > 1)
     w = optimum_w(dataset, k, w, cycle)
 
-  # Step 7. Predict the 'n.ahead' next values for the time series.
-  preds = as.vector(t(psf_predict(dataset, k, w, n.ahead, cycle)))[1:original.n.ahead]
+  # Step 7. Build the 'psf' object to be returned.
+  res = list(original_series = data, train_data = dataset, k = k, w = w, cycle = cycle, dmin = dmin, dmax = dmax)
 
-  # Step 8. Denormalize predicted data.
-  preds = preds * (dmax - dmin) + dmin
+  # Step 8. Assign class "psf" to returned object.
+  class(res) <- "psf"
 
-  # Step 9. Return both the predictions and the model.
-  res = list(predictions = preds, k = k, w = w)
   return(res)
 
 }
+
--- inst/doc/PSF_vignette.R
+++ inst/doc/PSF_vignette.R
@@ -2,18 +2,26 @@
 library(PSF)
 
 ## ------------------------------------------------------------------------
-a <- psf(data = nottem, n.ahead = 12)
-a
+nottem_model <- psf(nottem)
+nottem_model
 
 ## ------------------------------------------------------------------------
-b <- psf(data = sunspots, n.ahead = 48)
-b
+sunspots_model <- psf(sunspots)
+sunspots_model
+
+## ------------------------------------------------------------------------
+nottem_preds <- predict(nottem_model, n.ahead = 12)
+nottem_preds
+
+## ------------------------------------------------------------------------
+sunspots_preds <- predict(sunspots_model, n.ahead = 48)
+sunspots_preds
 
 ## ---- fig.width = 7, fig.height = 4--------------------------------------
-psf_plot(data = nottem, predictions = a$predictions)
+plot(nottem_model, nottem_preds)
 
 ## ---- fig.width = 7, fig.height = 4--------------------------------------
-psf_plot(data = sunspots, predictions = b$predictions)
+plot(sunspots_model, sunspots_preds)
 
 ## ------------------------------------------------------------------------
 library(PSF)
@@ -22,10 +30,10 @@ options(warn=-1)
   
 ## Consider data `sunspots` with removal of last years's readings
 # Training Data
-x <- sunspots[1:2772]
+train <- sunspots[1:2772]
 
 # Test Data
-y <- sunspots[2773:2820]
+test <- sunspots[2773:2820]
 
 PSF <- NULL
 ARIMA <- NULL
@@ -36,21 +44,22 @@ for(i in 1:5)
   set.seed(i)
   
   # for PSF
-  a <- psf(data = x, n.ahead = 48)$predictions
-  
+  psf_model <- psf(train)
+  a <- predict(psf_model, n.ahead = 48)
+
   # for ARIMA
-  b <- forecast(auto.arima(x), 48)$mean
+  b <- forecast(auto.arima(train), 48)$mean
   
   # for ets
-  c <- as.numeric(forecast(ets(x), 48)$mean)
+  c <- as.numeric(forecast(ets(train), 48)$mean)
   
   ## For Error Calculations
   # Error for PSF
-  PSF[i] <- sqrt(mean((y - a)^2))
+  PSF[i] <- sqrt(mean((test - a)^2))
   # Error for ARIMA
-  ARIMA[i] <- sqrt(mean((y - b)^2))
+  ARIMA[i] <- sqrt(mean((test - b)^2))
   # Error for ETS
-  ETS[i] <- sqrt(mean((y - c)^2))
+  ETS[i] <- sqrt(mean((test - c)^2))
 
 }
 
